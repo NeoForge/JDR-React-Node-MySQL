bf12369f92fcd1f9ee186cb173cb52e56f3502fd
LuckyTurtle <pro.nicolas.philippe@gmail.com>
2023-06-19 21:57:35 +0200

    Finish item controller inventory+equipement

--- Back/controllers/itemController.js
+++ Back/controllers/itemController.js
@@ -1,6 +1,10 @@
-import { models } from "../data/sequelize.js";
+import {models} from "../data/sequelize.js";
+
 const Item = models.item;
 const CharacterItem = models.character_item;
+const Equipement = models.equipment;
+const maxEquip = 5;
+
 class itemsController {
 
     async createItem(req, res) {
@@ -42,15 +46,15 @@ class itemsController {
 
     async updateItem(req, res) {
         try {
-            const stat_id = req.params.id;
+            const itemId = req.params.id;
             const { item_name, item_description, item_price, item_type, item_effect, item_rarity } = req.body;
-            const item = await Item.findByPk(item_id);
-            item.item_name = item_name,
-                item.item_description = item_description,
-                item.item_price = item_price,
-                item.item_type = item_type,
-                item.item_effect = item_effect,
-                item.item_rarity = item_rarity,
+            const item = await Item.findByPk(itemId);
+            item.item_name = item_name
+                item.item_description = item_description
+                item.item_price = item_price
+                item.item_type = item_type
+                item.item_effect = item_effect
+                item.item_rarity = item_rarity
                 await item.save();
             res.status(200).json(item);
         } catch (error) {
@@ -115,6 +119,23 @@ class itemsController {
         }
     }
 
+    async getOneItemFromInventory(req, res) {
+        try {
+            const characterId = req.params.id;
+            const itemId = req.body;
+            const characterItem = await CharacterItem.findAll({
+                where: {
+                    character_id: characterId,
+                    item_id: itemId
+                }
+            })
+            res.status(200).json(characterItem)
+            //add if() in front to check if item exist
+        } catch (error) {
+            res.status(400).json({ message: error.message });
+        }
+    }
+
     async deleteFromInventory(req, res) {
         try {
             const characterItemId = req.params.id;
@@ -123,6 +144,92 @@ class itemsController {
             res.status(200).json({ message: "Character item deleted" })
         } catch (error) {
             res.status(400).json({ message: error.message });
+        }
+    }
+
+    // async hasEquipementSpace(req, res) {
+    //     try {
+    //         const characterId = req.params.id;
+    //         const equipment = this.checkEquipement(characterId)
+    //         if (equipment.length>= maxEquip) {
+    //             res.status(200).json({ message: "Hep hep hep on ne triche pas !" })
+    //         }
+    //         res.status(200).json({ message: "Tu peux equiper des trucs" })
+    //
+    //     } catch (error) {
+    //         res.status(400).json({ message: error.message });
+    //     }
+    //
+    // }
+    
+    async checkEquipement(id){
+        return await Equipement.findAll({
+                where: {
+                    character_id: id,
+                }
+            }
+        );
+    }
+    
+    async addEquipementToChar(req, res) {
+        try {
+            const characterId = req.params.id;
+            const characterItemId = req.body;
+            const characterItem = await CharacterItem.findByPk(characterItemId);
+            if(characterItem.character_id === characterId && this.checkEquipement(characterId).length<5){
+                await Equipement.create({
+                    character_id: characterId,
+                    item_id: characterItem.item_id
+                })
+            }
+            if (characterItem.quantity>1){
+                characterItem.quantity--
+                characterItem.save()
+            }else{
+                characterItem.destroy()
+            }
+            res.status(200).json({message:"Kachink Equipé !"})
+
+        }catch (e) {
+            res.status(400).json({ message: e.message });
+        }
+    }
+
+    async removeEquipementFromChar(req, res){
+        try {
+            const characterId = req.params.id;
+            const equipementId = req.body;
+            const characterEquipement = await Equipement.findByPk(equipementId);
+            const itemExistInInventory = await CharacterItem.find({
+                where: {
+                    item_id: characterEquipement.item_id,
+                    character_id: characterId
+                }
+            })
+            if (itemExistInInventory) {
+                itemExistInInventory.quantity++;
+                itemExistInInventory.save()
+            } else {
+                CharacterItem.create({
+                    item_id: characterEquipement.item_id,
+                    character_id: characterId,
+                    quantity: 1
+                })
+            }
+            characterEquipement.destroy();
+            res.status(200).json({message: "Déséquipé !"})
+        } catch (e) {
+            res.status(400).json({ message: e.message });
+        }
+    }
+
+    async getAllEquipementFromChar(req, res){
+        try {
+            const characterId = req.params.id;
+            const stuff = this.checkEquipement(characterId);
+            res.status(200).json(stuff)
+        } catch (e) {
+            res.status(400).json({ message: e.message });
         }
     }
 }
--- Back/routes/itemRoute.js
+++ Back/routes/itemRoute.js
@@ -6,8 +6,12 @@ export const itemRoutes = (app) => {
     app.get("/item/all", Auth, ItemsController.getAllItem);
     app.get("/item/:id", Auth, ItemsController.getItemById);
     app.get("/inventory/all/:id", Auth, ItemsController.getAllInventoryFromChar);
+    app.get("/inventory/one/:id",Auth, ItemsController.getOneItemFromInventory);
+    app.get("/equipement/characterStuff/all/:id", Auth, ItemsController.getAllEquipementFromChar);
     app.post("/item/create", AuthAdmin, ItemsController.createItem);
     app.post("/inventory/create", Auth, ItemsController.addItemInventory);
+    app.post("/equipement/character/equip/:id", Auth, ItemsController.addEquipementToChar);
+    app.post("/equipement/character/remove/:id", Auth, ItemsController.removeEquipementFromChar);
     app.put("/item/update/:id", AuthAdmin, ItemsController.updateItem);
     app.put("/inventory/update/:id", Auth, ItemsController.updateInventory);
     app.delete("/item/delete/:id", AuthAdmin, ItemsController.deleteItem);
